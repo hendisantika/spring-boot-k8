26c631e24e47ac1dfe288f94d7958e420c408e46
hendisantika <hendisantika@yahoo.co.id>
2025-12-06 10:04:37 +0700

    feat(api): Add byId🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootk8/controller/PersonController.java
+++ src/main/java/id/my/hendisantika/springbootk8/controller/PersonController.java
@@ -6,10 +6,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -33,5 +35,13 @@ public class PersonController {
     public ResponseEntity<List<Person>> all() {
         List<Person> people = personService.findAll();
         return new ResponseEntity<>(people, HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Person> byId(@PathVariable Integer id) {
+        Optional<Person> optionalPerson = personService.findById(id);
+        return optionalPerson
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
