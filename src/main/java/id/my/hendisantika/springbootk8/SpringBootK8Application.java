c40bcbc3774219e81491e90a4150a89520389b91
hendisantika <hendisantika@yahoo.co.id>
2025-12-06 09:57:18 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootk8/SpringBootK8Application.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootk8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootK8Application {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringBootK8Application.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootk8/SpringBootK8ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootk8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootK8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
